ddf1017fdf66871b791f0d1102be906eea388610
Prithvi Sharan <18bce187@nirmauni.ac.in>
2021-08-27 13:03:22 +0530

    Two value case passed

--- src/stringcalc/Calculator.java
+++ src/stringcalc/Calculator.java
@@ -8,6 +8,9 @@ public class Calculator {
 		if(input.isEmpty()) {
 			return 0;
 		}
+		else if(input.length() == 1) {
+			return Integer.parseInt(input);
+		}
 		else {
 			return Integer.parseInt(num[0])+Integer.parseInt(num[1]);
 		}
